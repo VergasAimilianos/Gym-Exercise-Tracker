eb1eb631b11b0b9578c943a05810b268641ce316
VergasAimilianos <vergasaimil@gmail.com>
2026-06-12 11:40:50 +0300

    Preparing for Render deployment

--- fullstack/backend/src/main/java/com/Emil/CRUD/backend/config/WebConfig.java
+++ fullstack/backend/src/main/java/com/Emil/CRUD/backend/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                        .allowedOrigins("http://localhost:4200");
+                        .allowedOrigins("*");
             }
         };
     }
